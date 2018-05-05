de202aac7cf1088ad7853ee618975007362c1d48
Dean Babic <deanbabic@hotmail.com>
2018-05-05 23:21:49 +0200

    Update IOHandler.c

--- src/bin/IOHandler.c
+++ src/bin/IOHandler.c
@@ -8,7 +8,8 @@ void HexPrint(unsigned char* input, int length)
         printf("NULL");
     else
     {
-        for(int i = 0; i < length; i++)
+	int i;
+        for(i = 0; i < length; i++)
             printf("%02X ", *p++);
     }
     printf("\n");
